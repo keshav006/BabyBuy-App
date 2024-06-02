408035ef666e5a0bab1aec05336495560010a147
dreamit <ksavyadav06@gmail.com>
2024-06-03 01:25:42 +0545

    Initial Commit

---
+++ app/src/main/java/com/keshavproject/babybuyproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.keshavproject.babybuyproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
